1d7ac3fb4ec1dd9b13afb148b9cc24491b4e5bb8
Ryan Kara <ryan@rkara.us>
2021-08-09 00:48:35 -0400

    chore: switch to vercel

--- src/app/views/home/services/export.service.ts
+++ src/app/views/home/services/export.service.ts
@@ -11,7 +11,7 @@ export class ExportService {
 
   exportBlog$(userId: string): Observable<any> {
     return this.http
-      .get(`https://sparkpeople-backup.herokuapp.com/api/blogs/${userId}`, {
+      .get(`https://sparkpeople-export.vercel.app/api/blogs/${userId}`, {
         responseType: 'blob',
       })
       .pipe(
